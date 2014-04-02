e70fa87a7b41e98375171e435e5a9f6c0443fc91
Magnus <kjemag12@nith.no>
2014-04-02 13:26:39 +0200

    enigmajs forbedret

--- js/enigma.js
+++ js/enigma.js
@@ -1,19 +1,44 @@
 (function(){
 
-	//Deklarere variabler
+	// Elements
 	var container;
+	// Buttons
+	var loginBtn;
+	// Pages
+	var loginPage, mainPage;
 
-	var init = function(){
+	var init = function()
+	{
 		setObjects();
 		setEventHandlers();
 	}
 
-	var setObjects = function(){
+	var setObjects = function()
+	{
+		// Elements
 		container = document.getElementById("enigma");
+
+		// Buttons
+		loginBtn = document.getElementById("loginBtn");
+
+		// PageUrls
+		loginPage = "login.html";
+		mainPage = "main.html";
 	}
 
-	var setEventHandlers = function(){
-		
+	var setEventHandlers = function()
+	{
+		loginBtn.addEventListener("click", goTo, false);
+	}
+
+	var goTo = function()
+	{
+		switch(this.id)
+		{
+			case "loginBtn" :
+				window.location.assign(mainPage);
+				break;
+		}
 	}
 
 	window.onload = init;
