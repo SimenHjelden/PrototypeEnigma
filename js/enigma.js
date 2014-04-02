9e62e0f4bb37d84ad734dfac03dd89f74a4af6fb
Magnus <kjemag12@nith.no>
2014-04-02 12:24:54 +0200

    modified enigma.js and login.html, added css folder width main.css

---
+++ js/enigma.js
@@ -0,0 +1,21 @@
+(function(){
+
+	//Deklarere variabler
+	var container;
+
+	var init = function(){
+		setObjects();
+		setEventHandlers();
+	}
+
+	var setObjects = function(){
+		container = document.getElementById("enigma");
+	}
+
+	var setEventHandlers = function(){
+		
+	}
+
+	window.onload = init;
+
+})();
