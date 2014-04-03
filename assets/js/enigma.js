4adac5b8ca5e30c72e44acc5d7d8959336cfdd03
SimenHjelden <hjesim12@nith.no>
2014-04-03 14:38:53 +0200

    Fiksa ferdig at vi kan toogle passord felter på opprett rebus og fiksa litt på tilbake knapp

--- assets/js/enigma.js
+++ assets/js/enigma.js
@@ -1,7 +1,7 @@
 (function(){
 
 	// Elements
-	var container;
+	var container, txtPwd, txtRepeatPwd, pwd, repeatPwd, checkPwd;
 	// Buttons
 	var loginBtn, regBtn, backBtn;
 	// Pages
@@ -17,6 +17,11 @@
 	{
 		// Elements
 		container = document.getElementById("enigma");
+		txtPwd = document.getElementById("txtPwd");
+		txtRepeatPwd = document.getElementById("txtRepeatPwd");
+		pwd = document.getElementById("pwd");
+		repeatPwd = document.getElementById("repeatPwd");
+		checkPwd = document.getElementById("checkPwd");
 
 		// Buttons
 		loginBtn = document.getElementById("loginBtn");
@@ -31,7 +36,8 @@
 	}
 
 	var setEventHandlers = function()
-	{
+	{	
+		if(checkPwd != null){checkPwd.addEventListener("change", checkedPwd, false); }
 		if(loginBtn!=null){ loginBtn.addEventListener("click", login, false); }
 		if(regBtn!=null){ regBtn.addEventListener("click", function(){goTo(mainPage);}, false); }
 		if(backBtn!=null){ backBtn.addEventListener("click", function(){history.go(-1);}, false); }
@@ -47,7 +53,22 @@
 	{
 		window.location.assign(pageUrl);
 	}
-
+	
+	var checkedPwd = function(){
+		if(checkPwd.checked){
+			pwd.style.display = "block";
+			txtPwd.style.display = "block";
+			repeatPwd.style.display = "block";
+			txtRepeatPwd.style.display = "block";
+			}
+			else{
+				pwd.style.display = "none";
+				txtPwd.style.display = "none";
+				repeatPwd.style.display = "none";
+				txtRepeatPwd.style.display = "none";
+				}
+		}
+	
 	window.onload = init;
 
 })();
