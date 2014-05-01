e7164ab62e364806b2588410c7a90a579cc9544b
Magnus <kjemag12@nith.no>
2014-05-01 16:38:36 +0200

    selected-rebus map with posts

--- assets/js/gMap.js
+++ assets/js/gMap.js
@@ -1,3 +1,4 @@
+var randLat, randLng, randomNumber;
 function initialize() {
 
   navigator.geolocation.getCurrentPosition(function(position){
@@ -16,6 +17,20 @@ function initialize() {
       title: 'Click to zoom'
     });
 
+    if(location.pathname.substring(location.pathname.lastIndexOf("/") + 1) === "selected-rebus.html") {
+      for(var i = 0; i < 4; i++) {
+        createRandomLatLongCoord(position.coords.latitude, position.coords.longitude);
+        var image = '../assets/img/logo.png';
+              var myLatLng = new google.maps.LatLng(randLat, randLng);
+              var beachMarker = new google.maps.Marker({
+                  position: myLatLng,
+                  map: map,
+                  icon: image
+              });
+        $("tbody tr:nth-of-type("+ (i + 1) +") td:nth-of-type(4)").html("<p>lat: "+randLat+", lng: "+randLng+"</p>");
+      }
+    }
+
     google.maps.event.addListener(marker, 'click', function() {
       map.setZoom(8);
       map.setCenter(marker.getPosition());
@@ -28,6 +43,27 @@ function initialize() {
       });
 
   });
+}
+
+var createRandomLatLongCoord = function(lat, lng){
+  randBoolean();
+  if(randomNumber) {
+    randLat = (lat + (Math.random() * (0.002 - 0.005) + 0.005)).toFixed(4);
+  } else {
+    randLat = (lat - (Math.random() * (0.002 - 0.005) + 0.005)).toFixed(4);
+  }
+
+  randBoolean();
+  if(randomNumber) {
+    randLng = (lng + (Math.random() * (0.002 - 0.005) + 0.005)).toFixed(4);
+  } else {
+    randLng = (lng - (Math.random() * (0.002 - 0.005) + 0.005)).toFixed(4);
+  }
+  
+  
+}
+var randBoolean = function(){
+  randomNumber = Math.random() >= 0.5;
 }
 
 google.maps.event.addDomListener(window, 'load', initialize);
